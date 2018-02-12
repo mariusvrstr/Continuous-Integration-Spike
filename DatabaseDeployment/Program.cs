a1b1c3ec322f314fa4ce9fb40cec54ce896b5a5c
Marius Vorster <marius.vrstr@gmail.com>
2018-02-12 15:06:27 +0200

    Added DBUp project

---
+++ DatabaseDeployment/Program.cs
@@ -0,0 +1,45 @@
+﻿
+namespace DatabaseDeployment
+{
+    using DbUp;
+    using System;
+    using System.Linq;
+    using System.Reflection;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var connectionString =
+        args.FirstOrDefault()
+        ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+
+                #if DEBUG
+                    Console.ReadLine();
+                #endif
+
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
